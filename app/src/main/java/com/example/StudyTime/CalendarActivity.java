220de21d0d0e2feced5bd686aba7ad98a90c1e49
jandyrae <jandyrae@gmail.com>
2021-03-15 17:45:02 -0700

    adding Calendar, Timer (Main Activity), and xml for both

--- app/src/main/java/com/example/StudyTime/CalendarActivity.java
+++ app/src/main/java/com/example/StudyTime/CalendarActivity.java
@@ -1,17 +1,51 @@
 package com.example.StudyTime;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
+import java.util.Locale;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class CalendarActivity extends AppCompatActivity {
+    Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+    CalendarView calendarView;
+    TextView myDate;
+    String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        createEventOnCalendar();
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                date =  (month + 1) + "/" + dayOfMonth + "/" + year;
+                myDate.setText(date);
+
+            }
+        });
+
+    }
+
+    private void createEventOnCalendar() {
+
+        System.out.println("The Current Date is:" + calendar.getTime());
+        System.out.println("Current Calendar's Year: " + calendar.get(Calendar.YEAR));
+        System.out.println("Current Calendar's Day: " + calendar.get(Calendar.DATE));
+        System.out.println("Current MINUTE: " + calendar.get(Calendar.MINUTE));
+        System.out.println("Current SECOND: " + calendar.get(Calendar.SECOND));
+//        myDate.setText((CharSequence) calendar.getTime());
     }
 
     public void moveToTimer(View view) {
--- app/src/main/java/com/example/StudyTime/MainActivity.java
+++ app/src/main/java/com/example/StudyTime/MainActivity.java
@@ -4,16 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Chronometer;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Chronometer simpleTimer;
+    private boolean running;
+    private long pauseOffset; // use to calculate time paused
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        // initiate views
+        simpleTimer = (Chronometer) findViewById(R.id.simpleTimer);
+        simpleTimer.setFormat("Time: %s");
+        simpleTimer.setBase(SystemClock.elapsedRealtime());
 
+        simpleTimer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                if ((SystemClock.elapsedRealtime() - chronometer.getBase())>= 60000){
+//                line below would stop the clock at the above set time (maybe after set amount of hours)
+//                chronometer.setBase(SystemClock.elapsedRealtime());
+//                give a message at the above time (maybe a time to take a quick break)
+                    Toast.makeText(MainActivity.this, "Great Start, 1 minute down!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
     public void moveToCalendar(View view) {
         Intent intent = new Intent(this, CalendarActivity.class);
         startActivity(intent);
@@ -22,5 +45,29 @@ public class MainActivity extends AppCompatActivity {
     public void moveToAddSession(View view) {
         Intent intent = new Intent(this, AddSessionActivity.class);
         startActivity(intent);
+    }
+
+    public void startTimer(View view) {
+        // if not running this will start the timer (and subtract the time from stopped/paused)
+        if (!running){
+            simpleTimer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            simpleTimer.start();
+            running = true;
+        }
+    }
+
+    public void pauseTimer(View view) {
+        // no way to stop only pause, this will take the time and subtract the time stopped
+        if (running) {
+            simpleTimer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - simpleTimer.getBase();
+            running = false;
+        }
+    }
+
+    public void resetTimer(View view) {
+        //     stops the timer and clears the paused hold so it will truly reset
+        simpleTimer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
     }
 }
