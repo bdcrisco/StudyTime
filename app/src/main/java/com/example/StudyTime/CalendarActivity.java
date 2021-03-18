c8e35ba1a2190b31231840c573c1a6e2f2622ee1
jandyrae <jandyrae@gmail.com>
2021-03-17 18:38:01 -0700

    working timePickerDialog commented out, using submitTime with button to get time needed for Session

--- app/src/main/java/com/example/StudyTime/CalendarActivity.java
+++ app/src/main/java/com/example/StudyTime/CalendarActivity.java
@@ -3,56 +3,78 @@ package com.example.StudyTime;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-import com.applandeo.materialcalendarview.CalendarView;
-import com.applandeo.materialcalendarview.EventDay;
-import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
-
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Locale;
 
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class CalendarActivity extends AppCompatActivity {
-    com.applandeo.materialcalendarview.CalendarView calendarView;
-    EventDay currentDay;
-    SessionList sessionList = SessionList.getInstance();
-
     Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+    CalendarView calendarView;
     TextView myDate;
     String date;
+    Button getTime;
+    int hour,  min;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        calendarView = (com.applandeo.materialcalendarview.CalendarView) findViewById(R.id.Calendar);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
         myDate = (TextView) findViewById(R.id.myDate);
+        getTime = findViewById(R.id.timeButton);
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        min = calendar.get(Calendar.MINUTE);
 
-        myDate.setText(new SimpleDateFormat("M/dd/yyyy").format(calendar.getTime()));
-
+        myDate.setText(new SimpleDateFormat("MM/dd/yyyy").format(calendar.getTime()));
         createEventOnCalendar();
 
-        OnDayClickListener onDayClick = new OnDayClickListener() {
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onDayClick(EventDay eventDay) {
-                myDate.setText(eventDay.toString());
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                date =  (month + 1) + "/" + dayOfMonth + "/" + year;
+                myDate.setText(date);
+
             }
+        });
 
-//            @Override
-//            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-//                date =  (month + 1) + "/" + dayOfMonth + "/" + year;
-//                myDate.setText(date);
-//            }
-//        });
+        getTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        };
+//                TimePickerDialog timePickerDialog = new TimePickerDialog(CalendarActivity.this, 1, new TimePickerDialog.OnTimeSetListener() {
+//                    @Override
+//                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//                        Toast.makeText(CalendarActivity.this, ""+hourOfDay+":"+minute, Toast.LENGTH_SHORT).show();
+//                    }
+//                }, hour, min, false);
+//                timePickerDialog.show();
+            }
+        });
+    }
+    public void submitTime(View view){
+        TimePicker timePicker = new TimePicker(CalendarActivity.this);
+        long returnTime = calendar.getTime().getTime();
+        returnTime += (timePicker.getHour() * 3600 + timePicker.getMinute() * 60);
+        java.sql.Timestamp sessionStart = new Timestamp(returnTime);
+        Session adjSession = new Session();
+        adjSession.setStartTime(sessionStart);
+        System.out.println(adjSession);
     }
 
     private void createEventOnCalendar() {
@@ -74,4 +96,6 @@ public class CalendarActivity extends AppCompatActivity {
         Intent intent = new Intent(this, RecyclerActivity.class);
         startActivity(intent);
     }
+
+
 }
