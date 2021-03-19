9f5aba1830584355ce7b8db4ad612d4f2d54691f
RicardoLeonCo <leo19014@byui.edu>
2021-03-18 22:02:04 -0600

    This is my commit

--- app/src/main/java/com/example/StudyTime/MainActivity.java
+++ app/src/main/java/com/example/StudyTime/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+     
+        HamburgerFragment hamburgerfragment = new HamburgerFragment("param1", "param2");
     }
 
     public void moveToCalendar(View view) {
