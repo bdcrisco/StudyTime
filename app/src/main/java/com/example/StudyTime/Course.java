c6e4f33c5ca00fc13b59e0e93120c752d4f14ef7
cbrisco <cbrisco@byui.edu>
2021-04-03 16:34:50 -0600

    Fixed the onDayClick in the calendar to display the correct sessionList

--- app/src/main/java/com/example/StudyTime/Course.java
+++ app/src/main/java/com/example/StudyTime/Course.java
@@ -1,12 +1,15 @@
 package com.example.StudyTime;
 
+/*A class containing course information*/
 public class Course {
     private String courseName;
 
+    // constructor taking in a courseName
     public Course(String courseName) {
         this.courseName = courseName;
     }
 
+    // get function for a courseName as a string
     public String getCourseName() {
         return courseName;
     }
